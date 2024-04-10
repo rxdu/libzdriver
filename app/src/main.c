1e9ae35c7fb0c4a49d08c3dd1fb015c3052e3344
Ruixiang Du <ruixiang.du@gmail.com>
2024-04-10 13:29:50 +0800

    app: blink led sample works

--- app/src/main.c
+++ app/src/main.c
@@ -5,22 +5,42 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/drivers/gpio.h>
 
 #include <app_version.h>
 
-LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
+LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL
+);
 
-#define BLINK_PERIOD_MS_STEP 100U
-#define BLINK_PERIOD_MS_MAX  1000U
+#define LED1_NODE DT_ALIAS(led1)
+#define LED2_NODE DT_ALIAS(led2)
+#define LED3_NODE DT_ALIAS(led3)
 
-int main(void)
-{
-	printk("Zephyr Example Application %s\n", APP_VERSION_STRING);
+static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
+static const struct gpio_dt_spec led3 = GPIO_DT_SPEC_GET(LED3_NODE, gpios);
 
-	while (1) {
-		k_sleep(K_MSEC(100));
-	}
+int main(void) {
+    printk("Zephyr Example Application %s\n", APP_VERSION_STRING);
 
-	return 0;
+    if (!gpio_is_ready_dt(&led1) || !gpio_is_ready_dt(&led2) || !gpio_is_ready_dt(&led3)) {
+        return -1;
+    }
+
+    if (gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE) < 0 || gpio_pin_configure_dt(&led2, GPIO_OUTPUT_ACTIVE) < 0 ||
+        gpio_pin_configure_dt(&led3, GPIO_OUTPUT_ACTIVE) < 0) {
+        printk("Error: failed to configure LED device\n");
+        return -1;
+    }
+
+    while (1) {
+        gpio_pin_toggle_dt(&led1);
+        gpio_pin_toggle_dt(&led2);
+        gpio_pin_toggle_dt(&led3);
+
+        k_sleep(K_MSEC(500));
+    }
+
+    return 0;
 }
 
